d965049fa825171638ae46352437f21443beb827
Estefanie <Estefanieaparecida0@gmail.com>
2021-08-26 11:40:53 -0300

    Adicao dos tests

--- src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,6 +3,7 @@ package com.kazale.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.ActiveProfiles;
 
 @EnableCaching
 @SpringBootApplication
